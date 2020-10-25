c3a23434c4fc043622ce0e3e3ab136230cd0952d
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-10-25 09:53:39 +0100

    user authentication complete

--- src/App.js
+++ src/App.js
@@ -99,19 +99,20 @@ function App() {
             displayName: username,
           })
         })
-    //update & show the username
-        
+        //update & show the username
         .catch((error) => alert(error.message))
+        //close the modal after signin in
+        setOpen(false);
   }
 
   const signIn = (event) => {
     // this prevents a refresh when you submit the form
     event.preventDefault();
 
-    //
+    // 
     auth.signInWithEmailAndPassword(email, password)
         .catch((error) => alert(error.message))
-        
+      // close the modal after sigin in  
       setOpenSignIn(false);  
   }
 
@@ -158,6 +159,37 @@ function App() {
     </div>
     </Modal>
     
+    <Modal
+      open={openSignIn}
+      onClose={() => setOpenSignIn(false)}
+    >
+
+    <div style={modalStyle} className={classes.paper}>
+      <form className="app__signup">
+        <center>
+          <img
+          className="app__headerImage" 
+          alt=""
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          />
+        </center>   
+          <Input
+          placeholder="email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} 
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          <Button type="submit" onClick={signIn}>Sign In</Button>
+        
+      </form>
+        </div>
+    </Modal>
     
       <div className="app__header">
         <img
@@ -165,14 +197,15 @@ function App() {
           alt=""
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
         />
-      </div>
+      </div> 
 
       {user ? (
         <Button onClick={() => auth.signOut()}>Logout</Button>
 
       ) : (
         <div className="app__loginContainer">
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+        <Button onClick={() => setOpen(true)}>Sign Up</Button>
         </div>
       )}
       
